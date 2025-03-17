591b895df782dbd69382d789820a6efbba0c590a
ShinCheolHwan-daou <tlscjfghks@daou.co.kr>
2025-03-17 17:00:03 +0900

    Add files via upload

---
+++ [C]250317-실습36번.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    union year {
+        int year;
+        int grade;
+    };
+    typedef struct student {
+        char name[20];
+        char sex;
+        int stid;
+        union year y;
+    } ST;
+
+    ST st1 = {"kdhong", 'm', 1508001};
+    st1.y.grade = 4;
+    printf("%s 학생은 %d학년입니다\n", st1.name, st1.y);
+    printf("%s 학생은 %d학년입니다\n", st1.name, st1.y.grade);
+    printf("%s 학생은 %d학년입니다\n", st1.name, st1.y.year);
+    st1.y.year = 1;
+    printf("%s 학생은 %d학년입니다\n", st1.name, st1.y);
+    printf("%s 학생은 %d학년입니다\n", st1.name, st1.y.grade);
+    printf("%s 학생은 %d학년입니다\n", st1.name, st1.y.year);
+}
